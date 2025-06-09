80e730352e51cb119959e9b80c3babe8dbd38f19
kacp-k01 <kacpkuc@gmail.com>
2025-06-09 23:07:22 +0200

    initial commit

--- portfolio-optimizer/services/preditctor.py
+++ portfolio-optimizer/services/preditctor.py
@@ -1,5 +1,19 @@
-def predict_future(model, X, y, scaler):
+import numpy as np
+
+def predict_future(model, X, y, scaler, forecast_days=1):
     predicted_scaled = model.predict(X)
-    predicted = scaler.inverse_transform(predicted_scaled)
+    scaler.inverse_transform(predicted_scaled)
     actual = scaler.inverse_transform(y.reshape(-1, 1))
-    return predicted.flatten(), actual.flatten()
+
+    # Ekstrapolacja predykcji o N dni
+    last_input = X[-1]
+    future_predictions = []
+    current_input = last_input
+
+    for _ in range(forecast_days):
+        next_pred = model.predict(current_input.reshape(1, -1, 1))
+        future_predictions.append(next_pred[0, 0])
+        current_input = np.append(current_input[1:], next_pred[0, 0])
+
+    future_predictions = scaler.inverse_transform(np.array(future_predictions).reshape(-1, 1))
+    return future_predictions.flatten(), actual.flatten()
